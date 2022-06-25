a3199feec37c1cc7c2fbc12267a3661f650dd45f
Melanie0522 <106500812+Melanie0522@users.noreply.github.com>
2022-06-25 11:15:45 +0800

    commit java

---
+++ src/Student/main.java
@@ -0,0 +1,8 @@
+package Student;
+
+public class main {
+    public static void main(String[] args) {
+        Student stu_1 = new Student("Mary",1,"girl",6);
+        System.out.println(stu_1.getName()+" "+stu_1.getNO()+" "+stu_1.getGender()+" "+stu_1.getAge());
+    }
+}
